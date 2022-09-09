b46a3f43645e460c32ce1f83bee9829de3ec0741
codebydolapo <bashorun115@gmail.com>
2022-09-09 01:55:42 -0700

    wrote unit tests

--- src/contracts/SequinCoin.sol
+++ src/contracts/SequinCoin.sol
@@ -4,10 +4,19 @@ import "./ERC721Full.sol";
 
 contract SequinCoin is ERC721Full {
 
-  constructor() ERC721Full("Sequincoin", "SEQ") public {
+  constructor() ERC721Full("SequinCoin", "SEQ") public {
     
   }
 
+  function mint(address _to, string memory tokenURI) public returns(bool){
+    uint356 _tokenId = totalSupply.add(1);
+    _mint(_to, _tokenId);
+
+    _setTokenURI(_tokenId, tokenURI);
+
+    return true;
+
+  }
 
 
 }
--- test/SequinCoin.test.js
+++ test/SequinCoin.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const SequinCoin = artifacts.require('./SequinCoin.sol')
 
 require('chai')
@@ -5,5 +7,40 @@ require('chai')
   .should()
 
 contract('SequinCoin', (accounts) => {
+  let token
   
+  before(async()=>{
+    token = await SequinCoin.deployed()
+  })
+  
+  describe('deployment', async () => {
+
+    it('deploys successfully', async () => {
+      const address = await token.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address, "")
+      assert.notEqual(address, null)
+      assert.notEqual(address, undefined)
+    })
+
+    it('has a name', async()=>{
+      const name = await token.name()
+      assert.notEqual(name, undefined)
+      assert.notEqual(name, null)
+      assert.notEqual(name, "")
+    })
+
+    it('has a symbol', async()=>{
+      const symbol = await token.symbol()
+      assert.notEqual(symbol, undefined)
+      assert.notEqual(symbol, null)
+      assert.notEqual(symbol, "")
+    })
+
+    describe('token distribution', async()=>{
+      await token.mint()
+    })
+
+  })
+
 })
