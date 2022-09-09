1c3be6d4942f2655658a2cdb293798552d2e6823
codebydolapo <bashorun115@gmail.com>
2022-09-08 18:41:28 -0700

    initialized token constructor

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -2,7 +2,13 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Full.sol";
 
-contract MemoryToken is ERC721Full {
-  // Code goes here...
+contract SequinCoin is ERC721Full {
+
+  constructor() ERC721Full("Sequincoin", "SEQ") public {
+    
+  }
+
+
 
 }
+
